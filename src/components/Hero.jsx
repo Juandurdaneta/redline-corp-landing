34a05ec8db899e3853aef8078d12aa2247db5111
Juandurdaneta <juandurdanet@gmail.com>
2025-04-05 14:23:11 -0400

    refactor Hero component layout and styles; add new font styles and adjust spacing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,39 @@ import React, { useState } from "react";
 
 const Hero = () => {
   const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
       {isLoading && <div>LOADING</div>}
-      <div
-        id="#main-frame"
-        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg"
-      >
+
+      <div className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg">
         <img
           src="img/hero.png"
           alt="hero"
           className="absolute left-0 top-0 size-full object-cover object-center"
         />
 
-        <h1 className="special-font hero-heading absolute z-40 uppercase text-end left-30 top-30 text-red-dark">
-          <b>The</b> ultimate <br /> refreshing <br />
-          <b>ride</b>
-        </h1>
+        {/* <h1 className="hero-heading absolute right-[5vw] top-[20vh] z-40 text-red-dark special-font">
+          <span className=" word-spacing-close">
+            <b>The</b>
+          </span>
+          ultimate
+          <br />
+          refreshing
+          <br />
+          <span className="">
+            <b>ride</b>
+          </span>
+        </h1> */}
+
+        <div className="special-font hero-heading text-red-dark relative text-end w-screen h-screen z-40">
+          <h1 className="absolute top-[25vh] right-[6vw] flex gap-0 items-end">
+            <b>The</b> ultimate
+          </h1>
+          <h1 className="absolute top-[34vh] left-[8vw] leading-[0.5]">
+            refreshing <br /> <b className="p-10">ride</b>
+          </h1>
+        </div>
       </div>
     </div>
   );
