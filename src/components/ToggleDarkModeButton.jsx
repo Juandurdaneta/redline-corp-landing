61ee9d929683dce1bca0f79e4c4b4dab60ffe0c7
Juandurdaneta <juandurdanet@gmail.com>
2025-04-06 13:11:14 -0400

    implement dark mode toggle functionality; update ToggleDarkModeButton component and styles in index.css; modify index.html for dark class support

--- src/components/ToggleDarkModeButton.jsx
+++ src/components/ToggleDarkModeButton.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoonIcon from "../assets/moon.svg?react";
 import SunIcon from "../assets/sun.svg?react";
 import ButtonShape from "../assets/rectangle-5.svg?react";
 const ToggleDarkModeButton = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    // If darkMode is true, add .dark
+    // else remove it
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
+  // Toggle function
+  const handleToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <button className="flex items-center justify-center gap-2 rounded-full p-2 shadow-md transition-all duration-300 dark:text-white">
-      <ButtonShape className="absolute h-3/4 w-full animate-pulse opacity-30" />
+    <button
+      onClick={handleToggle}
+      className="flex items-center justify-center gap-2 rounded-full p-2 shadow-md transition-all duration-300 dark:text-white"
+    >
+      <ButtonShape className="absolute h-3/4 w-full animate-pulse dark:text-red-dark text-white" />
       <MoonIcon className="size-[20px]" />
     </button>
   );
