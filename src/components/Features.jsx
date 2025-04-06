0ca5cd4da03f94b81eac777ee5dd142d95ecf4c6
Juandurdaneta <juandurdanet@gmail.com>
2025-04-06 16:27:49 -0400

    feat: add Features component and integrate it into App layout; update special-font utility in CSS

---
+++ src/components/Features.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Features = () => {
+  return (
+    <section className="bg-black pb-52 px-8">
+      <div className="">
+        <h1 className="special-font text-6xl text-start text-white dark:text-red-dark">Features</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Features;
